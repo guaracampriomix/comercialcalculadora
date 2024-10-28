e4a52443a274d781170b43ff7b3afca2fb838653
guaracampriomix <137410666+guaracampriomix@users.noreply.github.com>
2024-10-28 13:46:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function atualizarValor() {
     const valorUnitario = document.getElementById("valor-unitario");
     const produtoSelecionado = produtoSelect.value;
 
-    //  produtos
+    // Produtos com seus respectivos valores unitários, litros e packs
     const produtos = {
         "1,5L": { unitario: 2.631578947, litros: 9, pack: 6 },
         "Água 12": { unitario: 1.789473684, litros: 6.12, pack: 12 },
@@ -21,7 +21,7 @@ function atualizarValor() {
     const produto = produtos[produtoSelect.options[produtoSelect.selectedIndex].text];
     
     if (produto) {
-        valorUnitario.innerText = `Caixa Unitária: ${produto.unitario.toString()}`;
+        valorUnitario.innerText = `Caixa Unitária: ${produto.unitario}`;
         calcularCaixasUnitarias(); 
     } else {
         valorUnitario.innerText = "";
@@ -33,29 +33,30 @@ function calcularCaixasUnitarias() {
     const produtoSelect = document.getElementById("produto");
     const produtoSelecionado = produtoSelect.options[produtoSelect.selectedIndex].text;
 
+    // Produtos com valores unitários
     const produtos = {
-        "1,5L": { pack: 6 },
-        "Água 12": { pack: 12 },
-        "Água Galão": { pack: 1 },
-        "Bombona": { pack: 1 },
-        "Caixa 24": { pack: 24 },
-        "Caixa 36": { pack: 36 },
-        "Concentrado": { pack: 12 },
-        "Energetico 2L": { pack: 6 },
-        "Energetico Lata": { pack: 6 },
-        "Ginseng": { pack: 12 },
-        "Pet 250": { pack: 12 }
+        "1,5L": { unitario: 2.631578947 },
+        "Água 12": { unitario: 1.789473684 },
+        "Água Galão": { unitario: 5.847953216 },
+        "Bombona": { unitario: 1.461988304 },
+        "Caixa 24": { unitario: 2.000000000 },
+        "Caixa 36": { unitario: 3.000000000 },
+        "Concentrado": { unitario: 3.508771930 },
+        "Energetico 2L": { unitario: 3.508771930 },
+        "Energetico Lata": { unitario: 0.471929825 },
+        "Ginseng": { unitario: 1.754385965 },
+        "Pet 250": { unitario: 0.877192982 }
     };
 
     const produto = produtos[produtoSelecionado];
 
     if (produto && caixasFisicas > 0) {
-        const quantidadeUnitarias = caixasFisicas * produto.pack;
-        document.getElementById("resultado").innerText = `Quantidade de Caixas Unitárias: ${quantidadeUnitarias}`;
+        const quantidadeUnitarias = caixasFisicas * produto.unitario;
+        document.getElementById("resultado").innerText = `Quantidade de Caixas Unitárias: ${quantidadeUnitarias.toFixed(2)}`;
     } else {
         document.getElementById("resultado").innerText = "";
     }
 }
 
-
+// Adiciona o evento ao seletor de produto
 document.getElementById("produto").addEventListener("change", atualizarValor);
